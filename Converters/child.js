8cdf48444c02fa0184df228303992adc0115c861
GavinC-org <61797100+GavinC-org@users.noreply.github.com>
2020-03-04 11:52:54 -0500

    Delete child.js

--- Converters/child.js
+++
@@ -1,9 +0,0 @@
-function cclick() {
-        let input = document.querySelector("input").value;
-        let a = input.replace(/R/g, "W")
-        let c = a.replace(/l/g, "w")
-        let b = c.replace(/L/g, "W")
-        let e = b.replace(/r/g, "w")        
-        document.querySelector("span").textContent = e;
-}
-
