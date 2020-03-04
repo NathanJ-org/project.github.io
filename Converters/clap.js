1b2aa5fe6b509b3bf1b9daea2afdf5d8301e725d
GavinC-org <61797100+GavinC-org@users.noreply.github.com>
2020-03-04 11:53:43 -0500

    Delete clap.js

--- Converters/clap.js
+++
@@ -1,7 +0,0 @@
-function cclick() {
-    let input = document.querySelector("input").value;
-    var d = input;
-    d = ' ' + d
-    var m = d.replace(/ /g, "👏")
-    document.querySelector("span").textContent = m;
-}
